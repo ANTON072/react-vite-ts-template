3a7490cc0d73689f5dacc2ad0d494776adbc17f7
Katsushi OUGI <ougi@to-r.net>
2024-02-06 10:06:02 +0900

    eslintの設定ができてなかったので修正

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,64 @@
 module.exports = {
   extends: ["airbnb", "airbnb-typescript", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  settings: {
+    // エイリアスの設定
+    "import/resolver": {
+      typescript: {
+        project: "./",
+      },
+      node: {
+        paths: ["src"],
+        extensions: [".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    // インポート順序
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "{react,react-dom/**,react-router-dom}",
+            group: "builtin",
+            position: "before",
+          },
+          {
+            pattern: "@/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+    // ファイル拡張子の指定を無効にする
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    // ReactをimportしなくてもJSXが使えるようにする
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+  },
 };
